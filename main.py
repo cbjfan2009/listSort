ff2ccf10a186d3006435611e5a35fd00dc12310a
cbjfan2009 <cbjfan2009@gmail.com>
2022-03-28 09:19:41 -0400

    Got the sorted() function sorting based on length of list for each tray.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import csv
 from tkinter import filedialog as fd
 
- #TODO Find and Access CSV list
+# OPEN CSV, EXTRACT HEADERS AND ROWS
 
 headers = []
 rows = []
@@ -28,7 +28,7 @@ for x in headers:
     print(f"Column headers: {x}")
 
 
- # find tray number position in list
+# find tray number position in list
 
 tray_number_index = 0
 for x in headers:
@@ -40,20 +40,22 @@ print(f"'Tray Number' index is: {tray_number_index}")
 
 
 
- #TODO Count pieces per tray and keep trays in order
+# loop through 'rows' to make dictionary of lists with tray_number as the key and associated addresses as values
 
 tray_count = 1
-dict = {}
+dict_trays = {}
 
 for row in rows:
-    if row[tray_number_index] not in dict.keys():
-        dict[row[tray_number_index]] = row
+    if row[tray_number_index] not in dict_trays.keys():
+        dict_trays[row[tray_number_index]] = row
     else:
-        dict[row[tray_number_index]].append(row)
+        dict_trays[row[tray_number_index]].append(row)
 
  # print tray number (x), number of items in the list (pieces in tray), the addresses in the tray.
-for x, y in dict.items():
+for x, y in dict_trays.items():
     print(x, len(y) ,y)
 
 
-pass
+res = ' '.join(sorted(dict_trays, key = lambda key: len(dict_trays[key]), reverse=True))
+
+print("Here is the output of sorting trays by length of list (res) \n", res)
